9aa5b75c03cb4c975612e82276b83596af0f75a6
amri1978 <amine.amri@gmail.com>
2020-02-02 03:23:12 +0100

    1

---
+++ fire.py
@@ -0,0 +1,146 @@
+import cv2
+import os
+import sys
+import math
+
+################################################################################
+
+import tflearn
+from tflearn.layers.core import *
+from tflearn.layers.conv import *
+from tflearn.layers.normalization import *
+from tflearn.layers.estimator import regression
+
+################################################################################
+
+def construct_fireCNN (x,y):
+
+
+    network = tflearn.input_data(shape=[None, y, x, 3], dtype=tf.float32)
+
+    network = conv_2d(network, 64, 5, strides=4, activation='relu')
+
+    network = max_pool_2d(network, 3, strides=2)
+    network = local_response_normalization(network)
+
+    network = conv_2d(network, 128, 4, activation='relu')
+
+    network = max_pool_2d(network, 3, strides=2)
+    network = local_response_normalization(network)
+
+    network = conv_2d(network, 256, 1, activation='relu')
+
+    network = max_pool_2d(network, 3, strides=2)
+    network = local_response_normalization(network)
+
+    network = fully_connected(network, 4096, activation='tanh')
+
+    network = fully_connected(network, 4096, activation='tanh')
+
+    network = fully_connected(network, 2, activation='softmax')
+
+
+    # constuct final model
+
+    model = tflearn.DNN(network, checkpoint_path='fire',
+                        max_checkpoints=1, tensorboard_verbose=2)
+
+    return model
+
+################################################################################
+
+if __name__ == '__main__':
+
+################################################################################
+
+    # construct and display model
+
+    model = construct_fireCNN(224, 224)
+    print("Constructed Fire CNN ...")
+
+    model.load(os.path.join("model", "fire"),weights_only=True)
+    print("Loaded CNN network weights ...")
+
+################################################################################
+
+    # network input sizes
+
+    rows = 224
+    cols = 224
+
+    # display and loop settings
+
+    windowName = "HoverGames Challenge 1: Fight Fire with Flyers with NXP";
+    keepProcessing = True;
+
+################################################################################
+
+    if len(sys.argv) == 2:
+
+        # load video file from first command line argument
+
+        video = cv2.VideoCapture(sys.argv[1])
+        print("Loaded video ...")
+
+        # create window
+
+        cv2.namedWindow(windowName, cv2.WINDOW_NORMAL);
+
+        # get video properties
+
+        width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH));
+        height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = video.get(cv2.CAP_PROP_FPS)
+        frame_time = round(1000/fps);
+        out = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc('M','J','P','G'),10, (width,height))
+
+        while (keepProcessing):
+
+            # start a timer (to see how long processing and display takes)
+
+            start_t = cv2.getTickCount();
+
+            # get video frame from file, handle end of file
+
+            ret, frame = video.read()
+            if not ret:
+                print("... end of video file reached");
+                break;
+
+            # re-size image to network input size and perform prediction
+
+            small_frame = cv2.resize(frame, (rows, cols), cv2.INTER_AREA)
+            output = model.predict([small_frame])
+
+            # label image based on prediction
+
+            if round(output[0][0]) == 1:
+                cv2.rectangle(frame, (0,0), (width,height), (0,0,255), 50)
+                cv2.putText(frame,'FIRE',(int(width/16),int(height/4)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 2,(255,255,255),7,cv2.LINE_AA);
+            else:
+                cv2.rectangle(frame, (0,0), (width,height), (0,255,0), 50)
+                cv2.putText(frame,'CLEAR',(int(width/16),int(height/4)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 2,(255,255,255),7,cv2.LINE_AA);
+
+            # stop the timer and convert to ms. (to see how long processing and display takes)
+
+            stop_t = ((cv2.getTickCount() - start_t)/cv2.getTickFrequency()) * 1000;
+
+            # image display and key handling
+
+            cv2.imshow(windowName, frame);
+            out.write(frame);
+
+            # wait fps time or less depending on processing time taken (e.g. 1000ms / 25 fps = 40 ms)
+
+            key = cv2.waitKey(max(2, frame_time - int(math.ceil(stop_t)))) & 0xFF;
+            if (key == ord('x')):
+                keepProcessing = False;
+            elif (key == ord('f')):
+                cv2.setWindowProperty(windowName, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN);
+    else:
+        print("usage: python fire.py videofile");
+    out.release()
+
+################################################################################
